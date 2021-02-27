38a1c81062cf026e8420ec4b0c756eb9dbd0088f
zapatran <zapatran@users.noreply.github.com>
2021-02-27 13:59:53 +0100

    update objects file with more examples

--- js/objects.js
+++ js/objects.js
@@ -1,22 +1,38 @@
 let car = {
-  model: "BMW",
-  color: "red",
+  model: 'BMW',
+  color: 'red',
   motor: 2.0,
   doors: 3,
 };
 
 for (let key in car) {
-  console.log("key", key, "value", car[key]);
+  console.log('key', key, 'value', car[key]);
 }
 
 delete car.doors;
 console.log(car);
 
 let student = {
-  name: "Andrea",
+  name: 'Andrea',
   sayMyName: function () {
     console.log("I'm " + this.name);
   },
 };
 
 // school.classRooms[0].students.push({ firstName: 'Celia', age: ?})
+
+'2h 22min'[('2h', '22min')][('2', '22')][(2, 22)][(120, 22)];
+142;
+
+const pelis = [{}, {}];
+
+function changeDurationStringToMinutes(movies) {
+  movies.sort();
+  // movies.forEach((movie) => {
+  //   movie.duration = calculateMinutes(movie.duration);
+  // });
+}
+
+changeDurationStringToMinutes(pelis);
+
+pelis; // [null, {}]
